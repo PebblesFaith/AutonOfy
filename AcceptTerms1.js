0a10e2305275c63d98424c0b7a379548ffb67288
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-05-14 15:04:49 -0500

    Update AcceptTerms1.js

--- AcceptTerms1.js
+++ AcceptTerms1.js
@@ -5,15 +5,3 @@ function goTerms() {
     
 }
 
-pdf.getPage(1).then( function(page) {
-
-    //We need to pass it a scale for "getViewport" to work
-    var scale = 1;
-
-    //Grab the viewport with original scale
-    var viewport = page.getViewport( 1 );
-
-    //Here's the width and height
-    console.log( "Width: 200px " + viewport.width + ", Height: 300px" + viewport.height );
-});
-
