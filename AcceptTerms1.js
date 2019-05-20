c491a5784548cf6df96a942645307d099fbe3fa0
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-05-20 13:38:29 -0500

    Update AcceptTerms1.js

--- AcceptTerms1.js
+++ AcceptTerms1.js
@@ -1,7 +1,8 @@
 var url = 'c:/Users/sarai/Documents/Module 2 Project/PDF/AutonOfy Terms and Conditions.pdf';
 
 function goTerms() { 
-    window.location.href = url;   
-    
+    window.location.href = url;       
 }
+
+alert("Do you agree to the 'Terms of Services?");
 
