2a0dca25946823f60b6a7b170c68a34cc249856d
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-06-28 00:13:36 -0500

    Update ConsumerSignUp.js
    After, I have learned the basic language in JavaScript and DOM API library from my on online course tutorial.  And, now I have the learning experience to write my users web application interactive webpage coded in JavaScript languages. As of June 28, 2019, I still have to apply my user forms validations and regular expression JavaScript language written in either jQuery or JavaScript, as well as comments.

--- ConsumerSignUp.js
+++ ConsumerSignUp.js
@@ -1,4 +1,101 @@
+function clearFunction() {
+    document.getElementById("firstName").value = ""; 
+    document.getElementById("firstName").placeholder = "First Name";    
+    document.getElementById("outputFirstName").innerHTML = "";
+
+    document.getElementById("middleName").value = "";   
+    document.getElementById("middleName").placeholder = "Middle Name";     
+    document.getElementById("outputMiddleName").innerHTML = "";
+
+    document.getElementById("lastName").value = "";   
+    document.getElementById("lastName").placeholder = "last Name";    
+    document.getElementById("outputLastName").innerHTML = "";
+
+    document.getElementById("usersDOB").value = "";
+    document.getElementById("outputUsersDOB").innerHTML = "";  
+
+    document.getElementById("usersBirthSex").value = "";      
+    document.getElementById("outputUsersBirthSex").innerHTML = "";
+
+    document.getElementById("usersGender").value = "";      
+    document.getElementById("outputUsersGender").innerHTML = "";
+
+    document.getElementById("usersId").value = ""; 
+    document.getElementById("usersId").placeholder = "Create User Id";    
+    document.getElementById("outputUsersId").innerHTML = "";
+
+    document.getElementById("usersEmailAddress").value = ""; 
+    document.getElementById("usersEmailAddress").placeholder = "Email Address";    
+    document.getElementById("outputUsersEmailAddress").innerHTML = "";
+
+    document.getElementById("usersConfirmEmailAddress").value = ""; 
+    document.getElementById("usersConfirmEmailAddress").placeholder = "Confirm Email Address";    
+    document.getElementById("outputConfirmUsersEmailAddress").innerHTML = "";
+
+    document.getElementById("usersPassword").value = ""; 
+    document.getElementById("usersPassword").placeholder = "Password";    
+    document.getElementById("outputUsersPassword").innerHTML = "";
+
+    document.getElementById("usersConfirmPassword").value = ""; 
+    document.getElementById("usersConfirmPassword").placeholder = "Password";    
+    document.getElementById("outputUsersConfirmPassword").innerHTML = "";    
+    
+}
+
+
 let today = new Date().toISOString().substr(0, 10);
 document.querySelector("#today").value = today;
+document.querySelector("#usersDOB").valueAsDate = new Date();
 
-document.querySelector("#today2").valueAsDate = new Date();
+/* Users input data are stored and displayed, as output data prints onto the 
+   guessing game webpage. */
+function submitFunction() {
+    var usersFirstName = document.getElementById("firstName").value;
+    document.getElementById("outputFirstName").innerHTML = usersFirstName;
+
+    var usersMiddleName = document.getElementById("middleName").value;
+    document.getElementById("outputMiddleName").innerHTML = usersMiddleName;
+
+    var usersLastName = document.getElementById("lastName").value;
+    document.getElementById("outputLastName").innerHTML = usersLastName;
+
+    var dobUsers = document.getElementById("usersDOB").value;
+    document.getElementById("outputUsersDOB").innerHTML = dobUsers;
+
+    var usersBirthSexChoice = document.getElementById("usersBirthSex").value;
+    document.getElementById("outputUsersBirthSex").innerHTML = usersBirthSexChoice;
+
+    var usersGenderChoice = document.getElementById("usersGender").value;
+    document.getElementById("outputUsersGender").innerHTML = usersGenderChoice;
+
+    var usersNameId = document.getElementById("usersId").value;
+    document.getElementById("outputUsersId").innerHTML = usersNameId;
+
+    var usersEmail = document.getElementById("usersEmailAddress").value;
+    document.getElementById("outputUsersEmailAddress").innerHTML = usersEmail;
+
+    var usersConfirmEmail = document.getElementById("usersConfirmEmailAddress").value;
+    document.getElementById("outputConfirmUsersEmailAddress").innerHTML = usersConfirmEmail;
+
+    var usersCreatePassword = document.getElementById("usersPassword").value;
+    document.getElementById("outputUsersPassword").innerHTML = usersCreatePassword;
+
+    var usersCreateConfirmPassword = document.getElementById("usersConfirmPassword").value;
+    document.getElementById("outputUsersConfirmPassword").innerHTML = usersCreateConfirmPassword;
+
+    if (usersCreatePassword != usersCreateConfirmPassword) {
+         alert("You first Passwords is not similar with 2nd password. Please enter same password in both");
+          return false; }
+           return true;
+    
+
+    
+}
+
+
+
+
+
+
+
+
